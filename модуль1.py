30ba1d8b9567f4f855713fbdd3a5ab147852d633
IcePhoenix09 <63466963+IcePhoenix09@users.noreply.github.com>
2020-04-11 20:45:45 +0300

    Roma

--- модуль1.py
+++ модуль1.py
@@ -1,7 +1,13 @@
 import pygame
 pygame.init()
+FPS = 60
+clock = pygame.time.Clock()
 screenx = 500
 screeny = 500
+radius = 10
+x_b = 100
+y_b = 100
+color_b = (100,100,100)
 window = pygame.display.set_mode((screenx, screeny))
 screen = pygame.Surface((screenx, screeny))
 run = True
@@ -9,9 +15,9 @@ while run:
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
             run = False
-    screen.fill((0, 10, 0))
+    screen.fill((0, 0, 0))
+    pygame.draw.circle(screen, color_b, (x_b, y_b), 50, 50)
     window.blit(screen, (0, 0))
     pygame.display.update()
+    clock.tick(FPS)
 pygame.quit()
-
-
