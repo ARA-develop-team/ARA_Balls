298c82162d1cd295e639579135b824e3137a37ec
Anton Yakovenko <62840267+Anton-Yakovenko@users.noreply.github.com>
2020-04-11 19:08:38 +0300

    Anton_first

--- модуль1.py
+++ модуль1.py
@@ -1,14 +1,31 @@
 import pygame
+
+def check_cube (xz, yz, rx, ry,):
+
+def check_sq (xz, yz, rx, ry, ):
+
 pygame.init()
 screenx = 500
 screeny = 500
 window = pygame.display.set_mode((screenx, screeny))
 screen = pygame.Surface((screenx, screeny))
+num = 20
+actioin = False
 run = True
 while run:
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
             run = False
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_SPACE]:
+        action = True
+
+    if  check_cube (xz, yz, rx, ry,) = True
+        num -= 1
+
+
+
     screen.fill((0, 10, 0))
     window.blit(screen, (0, 0))
     pygame.display.update()
