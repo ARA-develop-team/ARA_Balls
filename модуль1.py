0cad46ee8670123df988f25ea0ca61d20bf2ccd1
CapBot05 <capitanbot05@gmail.com>
2020-04-11 19:03:39 +0300

    Artem

--- модуль1.py
+++ модуль1.py
@@ -1,9 +1,18 @@
 import pygame
 pygame.init()
+
+rx=100
+ry=100
+widht1=20
+height1=20
+
 screenx = 500
 screeny = 500
 window = pygame.display.set_mode((screenx, screeny))
 screen = pygame.Surface((screenx, screeny))
+
+pygame.draw.rect(window, (255, 0, 0), (rx,ry,widht1,height1))
+
 run = True
 while run:
     for e in pygame.event.get():
